38a654563c806b7db1bfd799e1d0a29fcf052ddc
Salva Aleu <aleuwol12@gmail.com>
2026-09-14 13:19:49 +0200

    feat: load member portal styles

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import "./pages/leadership.css";
 import "./pages/work.css";
 import "./pages/communications.css";
 import "./pages/membership-media.css";
+import "./pages/portal.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
