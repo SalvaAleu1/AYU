920ba974f6ac42e5c1e1a96743f8d9a05a75ec5d
Salva Aleu <aleuwol12@gmail.com>
2026-09-14 13:50:53 +0200

    feat: load governance finance and constitution styles

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import "./pages/communications.css";
 import "./pages/membership-media.css";
 import "./pages/portal.css";
 import "./pages/portal-polish.css";
+import "./pages/governance-finance.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
