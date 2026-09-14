987c42727dd92ed4fa8fabe111830e369d4c60b9
Salva Aleu <aleuwol12@gmail.com>
2026-09-14 12:31:43 +0200

    feat: activate AYU router and phase 7-9 styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import Router from "./Router";
 import "./design/tokens.css";
 import "./styles.css";
 import "./pages/pages.css";
 import "./pages/leadership.css";
+import "./pages/work.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>,
 );
