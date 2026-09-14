dd73907237b03656c3a09df446a70c20c07ededb
Salva Aleu <aleuwol12@gmail.com>
2026-09-14 14:18:00 +0200

    Expose AYU website utilities in footer navigation

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -15,6 +15,7 @@ const footerLinks = [
   { href: "/?page=membership", label: "Membership" },
   { href: "/?page=contact", label: "Contact" },
   { href: "/?page=search", label: "Search" },
+  { href: "/?page=utilities", label: "Website Utilities" },
 ];
 
 const legalLinks = [
