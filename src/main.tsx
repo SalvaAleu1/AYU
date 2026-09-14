f93701c4422454b7985ae134d532a44b1f50e4f5
Salva Aleu <aleuwol12@gmail.com>
2026-09-14 13:06:33 +0200

    feat: load Media Centre and membership styles

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import "./pages/pages.css";
 import "./pages/leadership.css";
 import "./pages/work.css";
 import "./pages/communications.css";
+import "./pages/membership-media.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
