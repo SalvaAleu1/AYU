427f246534b6bdb184aa5c89d93f9105c10318c3
Salva Aleu <aleuwol12@gmail.com>
2026-09-14 12:20:46 +0200

    fix: load leadership profile layout styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./design/tokens.css";
 import "./styles.css";
 import "./pages/pages.css";
+import "./pages/leadership.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
